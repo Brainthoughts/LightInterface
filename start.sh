9d3c8fb443e160831870421c9277e7a37907c052
Brainthoughts <acniedner@gmail.com>
2021-12-10 23:52:13 -0800

    updated start and stop scripts

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
-
-kill $(pgrep -u $(whoami) node);
+pgrep -o -u $(whoami) node >/dev/null
+code=$?;
+[ $code -eq 0 ] && kill $(pgrep -u $(whoami) node);
 git pull;
 npm i;
 nohup node app.js &
--- stop.sh
+++ stop.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
-
-kill $(pgrep -u $(whoami) node);
+pgrep -o -u $(whoami) node >/dev/null
+code=$?;
+[ $code -eq 0 ] && kill $(pgrep -u $(whoami) node) || echo "node could not be found running as $(whoami)";
